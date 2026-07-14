71e0acad48bd81a9df5198ee60d85dedd400625a
aba-geomdan <abageomdan@gmail.com>
2026-07-14 10:44:44 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import obfuscator from "vite-plugin-javascript-obfuscator";
 // GitHub Pages: https://aba-geomdan.github.io/bip-maker/
 export default defineConfig({
   base: "/bip-maker/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscator({
+      apply: "build",
+      include: [/\.js$/],
+      exclude: [/node_modules/],
+      debugger: false,
+      options: {
+        compact: true,
+        stringArray: true,
+        stringArrayEncoding: ["base64"],
+        stringArrayThreshold: 0.75,
+        identifierNamesGenerator: "hexadecimal",
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        selfDefending: false,
+      },
+    }),
+  ],
 });
